ebc5dfcfa4e4746dbd14934d5a2b7d9f6d075f98
qf40805 <qf40805@mail.harpercollege.edu>
2024-10-27 12:05:26 -0500

    hide-and-seek-Fayaz

--- src/main/java/com/csc/FuzzyFinder.java
+++ src/main/java/com/csc/FuzzyFinder.java
@@ -1,8 +1,42 @@
 package com.csc;
 
+import java.util.ArrayList;
+
 public class FuzzyFinder {
-  // Your code goes here!
-  public static void main(String args[]) {
-    System.out.println("In addition to your search functions, you can write a main function if you wish!");
-  }
+    public static int linearSearch(ArrayList<Fuzzy> fuzzies) {
+        for (int i = 0; i < fuzzies.size(); i++) {
+            if (fuzzies.get(i).color.equals("gold")) {
+                return i;
+            }
+        }
+        return -1; 
+    }
+
+    public static int binarySearch(ArrayList<Fuzzy> fuzzies) {
+        int left = 0;
+        int right = fuzzies.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (fuzzies.get(mid).color.equals("gold")) {
+                return mid;
+            } else if (fuzzies.get(mid).color.compareTo("gold") < 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        FuzzyListGenerator generator = new FuzzyListGenerator();
+        
+        ArrayList<Fuzzy> randomizedFuzzies = generator.randomizedRainbowFuzzies();
+        int linearResult = linearSearch(randomizedFuzzies);
+        System.out.println("Linear Search (Randomized): Found 'gold' at index: " + linearResult);
+        
+        ArrayList<Fuzzy> sortedFuzzies = generator.sortedRainbowFuzzies();
+        int binaryResult = binarySearch(sortedFuzzies);
+        System.out.println("Binary Search (Sorted): Found 'gold' at index: " + binaryResult);
+    }
 }
